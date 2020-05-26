12fafe9b2bef088183d6d7041785d85d9e2ba088
Taku Nakai <t.nakai.office@gmail.com>
2020-05-26 09:20:30 +0900

    first commit

---
+++ tmp_pycode.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+import time
+from optparse import OptionParser
+
+sys.path.append(os.path.join("./"))
+from base import plot2d
+
+import logging
+logging.getLogger('matplotlib').setLevel(logging.ERROR)
+
+if __name__ == '__main__':
+    argvs = sys.argv
+    parser = OptionParser()
+    parser.add_option("--dir", dest="dir", default="./")
+    parser.add_option("--pxyz", dest="point",
+                      default=[0.0, 0.0, 0.0], type="float", nargs=3)
+    opt, argc = parser.parse_args(argvs)
+    print(opt, argc)
